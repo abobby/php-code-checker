222ee83c0e62ec91c45f0d299aa5afba29d0a352
Subhasish Ghosh <subhasishghosh@gmail.com>
2018-07-03 07:49:28 +0000

    Initial Commit

---
+++ src/Controller/PhpCodeChecker.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+/**
+ * This is the main class for PhpCodeChecker contains the common functions
+ */
+class PhpCodeChecker extends Controller { 
+
+    public function __construct(){
+
+    }
+
+    /**
+     * Default Index function for PhpCodeChecker
+     */
+    public function index() {
+
+    }
+}
